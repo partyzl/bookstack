f793dfef41f2ee4a8fcbbea06d732107b6a619dd
Parth Patel <parth6796@yahoo.com>
2021-10-08 15:11:57 +0100

    added user create method

---
+++ server/bookstack/users/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class UserRegistration(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True)
+    password_confirmation = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password", "password_confirmation")
+        write_only_fields = ('password', 'password_confirmation')
+    
+    def create(self, validated_data);
+        user = User.objects.create(
+            username=validated_data['username'],
+            email = validated_data['email']
+        )
+        password=validated_data['password']
+        password_confirmation=validated_data['password_confirmation']
+
+        if password != password_confirmation:
+            raise serializers.ValidationError({'password': "Your passwords don't match!"})
+        user.set_password(password)
+        user.save()
+
+        return user
