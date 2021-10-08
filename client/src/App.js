1791a064e74b8eab7861fa73743f2fcb6afb1533
Abigail McLeod-Peck <abigail.m.peck@gmail.com>
2021-10-08 15:20:13 +0100

    creating routes with react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/search">
+            <Search />
+          </Route>
+
+          <Route path="/library">
+            <Library />
+          </Route>
+
+          <Route path="/profile">
+            <Profile />
+          </Route>
+
+          <Route>
+            <NotFound404 />
+          </Route>
+        </Switch>
+      </NavBar>
     </div>
   );
 }
