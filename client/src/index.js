eb15540099d59a982f2d299220333c953a19a4d7
Abigail McLeod-Peck <abigail.m.peck@gmail.com>
2021-10-10 22:51:06 +0100

    nav and buttons

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
@@ -11,5 +11,5 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
