3f7c061d694e0a6ad9a551fde9f6e781d69dee4e
harrismawardi <harris.mawardi@yahoo.co.uk>
2021-10-11 09:11:20 +0100

    created books django app

--- server/bookstack/stats/views.py
+++ server/bookstack/stats/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import APIView, Response
 
 # Create your views here.
+class Stats(APIView):
+
+    def get():
+        return Response()
+
+    def put():
+        return Response()
