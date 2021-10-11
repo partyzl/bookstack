bac96fb5738ffe10b8abd741220d39d1817d07c8
harrismawardi <harris.mawardi@yahoo.co.uk>
2021-10-11 09:21:57 +0100

    created books model

--- server/bookstack/books/models.py
+++ server/bookstack/books/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.deletion import SET_NULL
+
 
 # Create your models here.
+class Book(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=500)
+    author = models.CharField(max_length=50)
+    cover = models.ImageField()
+    page_num = models.IntegerField()
+    rating = models.IntegerField()
+    publish_year = models.IntegerField()
+    date_started = models.IntegerField()
+    date_finished = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.title} by {self.author}'
+
+
+
