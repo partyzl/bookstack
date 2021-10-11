ec9f0fd4bf172779d7541ae4c92abcaf13fd4269
harrismawardi <harris.mawardi@yahoo.co.uk>
2021-10-11 17:56:09 +0100

    GET endpoint for user books/ book detail works

--- server/bookstack/books/views.py
+++ server/bookstack/books/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from .models import Book, BookStats
 from .serializers import BookSerializer, BookStatsSerializer
 from statistics import mean
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -17,12 +18,13 @@ class Books(APIView):
 class UserBooks(APIView):
     def get_object(self, username):
         try:
-            return Book.objects.get(user_id=username)
+            user = User.objects.get(username=username)
+            return Book.objects.get(user_id=user.id)
         except Book.DoesNotExist:
             raise Http404
 
     def get(self, request, username, format=None):
-        books = Book.objects.filter(user_id=username)
+        books = self.get_object(username)
         serializer = BookSerializer(books)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -35,16 +37,22 @@ class UserBooks(APIView):
 
 
 class UserBooksDetail(APIView):
-    def get(self, request, username, book_id, format=None):
+
+    def get_object(self, username, book_id):
         try:
-            book = Book.objects.get(user_id=username, id=book_id)
-            serializer = BookSerializer(book)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            user = User.objects.get(username=username)
+            return Book.objects.get(user_id=user.id, id=book_id)
         except Book.DoesNotExist:
             raise Http404
+    
+    def get(self, request, username, book_id, format=None):
+        book = self.get_object(username, book_id)
+        serializer = BookSerializer(book)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
 
     def put(self, request, username, book_id, format=None):
-        book = Book.objects.get(user_id=username, id=book_id)
+        book = self.get_object(username, book_id)
         title = book.title
         serializer = BookSerializer(book, data=request.data)
         if serializer.is_valid():
@@ -58,7 +66,7 @@ class UserBooksDetail(APIView):
         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, username, book_id, format=None):
-        book = Book.objects.get(user_id=username, id=book_id)
+        book = self.get_object(username, book_id)
         book.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
