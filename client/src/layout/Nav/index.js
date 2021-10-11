13b929c9a3797c6709f35d0a296d0544f1274b6d
jasminappleby <jasminapplebyholdham@hotmail.com>
2021-10-11 13:07:37 +0100

    navbar not fully responsive yet, still working on this for desktop and going to remove the hamburger button from mboile

--- client/src/layout/Nav/index.js
+++ client/src/layout/Nav/index.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { MdClose } from "react-icons/md"
+import { FiMenu } from "react-icons/fi"
 import "./styles.css";
+
 const Nav = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false)
+
+  const handleToggle = () => {
+    setNavbarOpen(!navbarOpen)
+  }
+
+  const closeMenu = () => {
+    setNavbarOpen(false)
+  }
+
   return (
     <div>
       <nav className="navbar fixed-bottom navbar-light">
-        <NavLink to="/home" activeClassName="current">
+
+      <button onClick={handleToggle} className="hamburger-button">
+        {navbarOpen ? (
+          <MdClose style={{ color: "black", width: "40px", height: "40px" }} />
+        ) : (
+          <FiMenu style={{ color: "black", width: "40px", height: "40px" }} />
+        )}
+      </button>
+
+        <NavLink to="/home" activeClassName="current" className={`menuNav ${navbarOpen ? " showMenu" : ""}`} onClick={() => closeMenu()} exact>
           <svg
   
             xmlns="http://www.w3.org/2000/svg"
@@ -17,8 +39,10 @@ const Nav = () => {
           >
             <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z" />
           </svg>
+          <h4 className="nav-text">Home</h4>
         </NavLink>
-        <NavLink to="/search" activeClassName="current">
+
+        <NavLink to="/search" activeClassName="current" className={`menuNav ${navbarOpen ? " showMenu" : ""}`} onClick={() => closeMenu()} exact>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
@@ -29,8 +53,10 @@ const Nav = () => {
           >
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
           </svg>
+          <h4 className="nav-text">Search</h4>
         </NavLink>
-        <NavLink to="/library" activeClassName="current">
+
+        <NavLink to="/library" activeClassName="current" className={`menuNav ${navbarOpen ? " showMenu" : ""}`} onClick={() => closeMenu()} exact>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
@@ -41,6 +67,7 @@ const Nav = () => {
           >
             <path d="M8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
           </svg>
+          <h4 className="nav-text">Library</h4>
         </NavLink>
       </nav>
     </div>
