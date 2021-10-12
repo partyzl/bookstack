a4e48a4ebe75dc0d4da3e1e6b4236979b19a6ddf
Parth Patel <parth6796@yahoo.com>
2021-10-12 12:36:26 +0100

    tried stuff for avatar post req

--- server/bookstack/profiles/serializers.py
+++ server/bookstack/profiles/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import Profile
 
+
 class ProfileSerializer(serializers.ModelSerializer):
+
+    avatar = serializers.FileField(max_length=None, use_url=True)
 
     class Meta:
         model = Profile
-        fields = ('avatar', 'fav_character', 'fav_book', 'book_target')
+        fields = ("avatar", "fav_character", "fav_book", "book_target")
--- server/bookstack/profiles/views.py
+++ server/bookstack/profiles/views.py
@@ -6,32 +6,41 @@ from stats.serializers import UserStatsSerializer
 from rest_framework import status
 from django.http import Http404
 from django.contrib.auth.models import User
+from rest_framework.parsers import FormParser, MultiPartParser
 
 # Create your views here.
 
+
 class CreateProfile(APIView):
-     def post(self, request, format=None):
+
+    parser_classes = (FormParser, MultiPartParser)
+
+    def post(self, request, format=None):
         prof_serializer = ProfileSerializer(data=request.data["profile"])
         stats_serializer = UserStatsSerializer(data=request.data["stats"])
         if prof_serializer.is_valid():
             prof_serializer.save()
-            stats_serializer.save()
-            return Response(prof_serializer.data, status=status.HTTP_201_CREATED)
-        return Response(prof_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            stats_serializer.save(avatar=request.data["profile"]["avatar"])
+            return Response(
+                prof_serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            prof_serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
 
 class Profiles(APIView):
-    
     def get_object(self, username):
         try:
-           user = User.objects.get(username=username)
-           return Profile.objects.get(user_id=user.id)
+            user = User.objects.get(username=username)
+            return Profile.objects.get(user_id=user.id)
         except Profile.DoesNotExist:
             raise Http404
 
     def get(self, request, username, format=None):
         profile = self.get_object(username)
         serializer = ProfileSerializer(profile)
-        return Response(serializer.data, status=status.HTTP_200_OK)        
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, username, format=None):
         profile = self.get_object(username)
