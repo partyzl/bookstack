2c31be3c28959a44ee97cdf1d8210f1960e265f3
jasminappleby <jasminapplebyholdham@hotmail.com>
2021-10-13 12:55:49 +0100

    made 500 error page and exported but have not added this to the app.js routes yet

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,4 +1,5 @@
 export { default as Error404 } from "./Error404"
+export { default as Error500 } from "./Error500"
 export { default as Home } from "./Home";
 export { default as LibraryLanding } from "./LibraryLanding";
 export { default as Login } from "./Login";
