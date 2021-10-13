6f4fb57f5cbdccf49d5d6c7ee4ab186bedf107a9
Abigail McLeod-Peck <abigail.m.peck@gmail.com>
2021-10-13 10:52:23 +0100

    overall styling changes

--- client/src/pages/LibraryLanding/index.js
+++ client/src/pages/LibraryLanding/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useHistory } from "react-router";
 import { Button, Nav } from "../../components";
+import "./styles.css";
 
 const LibraryLanding = () => {
   let history = useHistory();
