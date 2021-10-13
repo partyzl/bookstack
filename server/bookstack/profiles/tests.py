ef7aef439df1ec62e9277a3d583dbe7295e20e78
Parth Patel <parth6796@yahoo.com>
2021-10-13 16:37:39 +0100

    Testing if user profile endpoint works

--- server/bookstack/profiles/tests.py
+++ server/bookstack/profiles/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import RequestFactory, TestCase
+
+from .views import CreateProfile, Profiles
 
 # Create your tests here.
+class BaseTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username="test", email="test@test", password="testpass"
+        )
+
+
+class TestUserProfile(BaseTestCase):
+    def test_details(self):
+        req = self.factory.get("/test")
+        req.user = self.user
+
+        response = Profiles.as_view()(req)
+        self.assertEqual(response.status_code, 200)
