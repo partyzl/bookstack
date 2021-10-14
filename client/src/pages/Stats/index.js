970bf837be75a8199075c968cbe86556e4b47178
Tegan Gowing <tgowing97@gmail.com>
2021-10-14 09:50:07 +0100

    stats page shell

---
+++ client/src/pages/Stats/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { checkToken } from "../../actions/loginauth";
+
+const StatsDisplay = () => {
+  checkToken()
+
+  return (
+    <div>
+      
+    </div>
+  );
+};
+export default StatsDisplay;
