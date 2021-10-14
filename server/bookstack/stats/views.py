a6e96d1db69836be40076634f7b77b7e0c92846d
Parth Patel <parth6796@yahoo.com>
2021-10-14 09:50:23 +0100

    imported isAuth

--- server/bookstack/stats/views.py
+++ server/bookstack/stats/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from books.models import Book
 from .models import UserStats
 from .serializers import UserStatsSerializer
+from rest_framework.permissions import IsAuthenticated
 
 import json
 import math
@@ -15,7 +16,7 @@ class Stats(APIView):
 
     def get_objects(self, username):
         user = User.objects.get(username=username)
-        return UserStats.objects.get(user_id = user.id)
+        return UserStats.objects.get(user_id=user.id)
 
     def get(self, request, username, format=None):
         stats = self.get_objects(username)
@@ -33,52 +34,58 @@ class Stats(APIView):
             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-   
-
-
-#------------------------------HELPER FUNCTIONS-----------------------------#
+    # ------------------------------HELPER FUNCTIONS-----------------------------#
     def update_user_stats(self, user_id):
-            print(user_id)
-            finished_books = list(Book.objects.raw('SELECT * FROM books_book WHERE date_finished IS NOT NULL AND user_id_id IS %s',[user_id]))#insert query for books that have a not null finished date.
-            finished_total = len(finished_books)
-            [reading_time, pages_read] = self.get_reading_data(finished_books)
-            print(reading_time)
-            avg_book_length = round(pages_read / finished_total)
-            print(avg_book_length)
-            pages_per_day = round(pages_read / reading_time, 1) 
-            avg_book_time = round(reading_time / finished_total)
+        print(user_id)
+        finished_books = list(
+            Book.objects.raw(
+                "SELECT * FROM books_book WHERE date_finished IS NOT NULL AND user_id_id IS %s",
+                [user_id],
+            )
+        )  # insert query for books that have a not null finished date.
+        finished_total = len(finished_books)
+        [reading_time, pages_read] = self.get_reading_data(finished_books)
+        print(reading_time)
+        avg_book_length = round(pages_read / finished_total)
+        print(avg_book_length)
+        pages_per_day = round(pages_read / reading_time, 1)
+        avg_book_time = round(reading_time / finished_total)
 
-            genre_list = list(Book.objects.raw('SELECT id, genre, count(genre) FROM books_book GROUP by genre ORDER BY count(genre) DESC LIMIT 1'))
-            fav_genre = genre_list[0].__dict__["genre"]
-           
-            publish_years = list(Book.objects.raw('SELECT id, publish_year FROM books_book'))
-            decades_list = list(map(self.rounding, publish_years))
-            fav_era = self.most_common_decade(decades_list)
+        genre_list = list(
+            Book.objects.raw(
+                "SELECT id, genre, count(genre) FROM books_book GROUP by genre ORDER BY count(genre) DESC LIMIT 1"
+            )
+        )
+        fav_genre = genre_list[0].__dict__["genre"]
 
-            return ({
-                "pages_per_day":  pages_per_day,
-                "avg_book_time": avg_book_time,
-                "avg_book_length": avg_book_length,
-                "total_books_read": finished_total,
-                "fav_era": fav_era,
-                "genres": fav_genre
-              
-            })
+        publish_years = list(
+            Book.objects.raw("SELECT id, publish_year FROM books_book")
+        )
+        decades_list = list(map(self.rounding, publish_years))
+        fav_era = self.most_common_decade(decades_list)
 
+        return {
+            "pages_per_day": pages_per_day,
+            "avg_book_time": avg_book_time,
+            "avg_book_length": avg_book_length,
+            "total_books_read": finished_total,
+            "fav_era": fav_era,
+            "genres": fav_genre,
+        }
 
     def rounding(self, year_object):
         year = year_object.__dict__["publish_year"]
         return math.floor(year / 10) * 10
 
     def most_common_decade(self, decades):
-        counter = 0 
+        counter = 0
         decade = decades[0]
         for i in decades:
             current_decade = decades.count(i)
             if current_decade > counter:
                 counter = current_decade
                 decade = i
-        return decade                
+        return decade
 
     def get_reading_data(self, book_list):
         total_days = 0
