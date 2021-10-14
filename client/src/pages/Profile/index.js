78c48159879a6794c6c18e2d2e6ad5f1d1b2b83e
Abigail McLeod-Peck <abigail.m.peck@gmail.com>
2021-10-14 22:22:31 +0100

    avatar changes

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import { Nav, Button, ProfileSlider } from "../../components";
 import { useHistory } from "react-router";
 import { checkToken } from "../../actions/loginauth";
+import Avatar1 from "../../images/Avatars1.png";
+import Avatar2 from "../../images/Avatars2.png";
+import Avatar3 from "../../images/Avatars3.png";
+import Avatar4 from "../../images/Avatars4.png";
+import Avatar5 from "../../images/Avatars5.png";
+import Avatar6 from "../../images/Avatars6.png";
 import "./styles.css";
 
 const Profile = () => {
@@ -12,26 +18,36 @@ const Profile = () => {
   const setup = () => {
     history.push("/profilesetup");
   };
-  
+
   //  const [profile, setProfile] = useState(async () => await getProfile())
-  
+
   //  useEffect(async () => {
-  //   const statsData = await getStats() 
-  //   console.log(statsData) 
+  //   const statsData = await getStats()
+  //   console.log(statsData)
   //   setStats(statsData)
   //  }, [])
+
+  const getProfilePicture = () => {
+    const chosenAvatar = localStorage.getItem("avatar");
+    console.log("avatar", chosenAvatar);
+    if (chosenAvatar) {
+      if (chosenAvatar === "1") return Avatar1;
+      if (chosenAvatar === "2") return Avatar2;
+      if (chosenAvatar === "3") return Avatar3;
+      if (chosenAvatar === "4") return Avatar4;
+      if (chosenAvatar === "5") return Avatar5;
+      if (chosenAvatar === "6") return Avatar6;
+    } else {
+      return Avatar1;
+    }
+  };
 
   return (
     <div className="body">
       <Nav />
 
       {/* {profileimage} */}
-      <img
-        alt="profile picture"
-        src={`https://avatars.dicebear.com/api/gridy/${
-          Math.floor(Math.random() * 90000) + 10000
-        }.svg`}
-      />
+      <img alt="profile picture" src={getProfilePicture()} />
 
       {/* username */}
       <h4 className="introduction">Hello, {username}, got any new reads?</h4>
