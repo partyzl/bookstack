a40069123da8cea1ed14d861495752f46f9a9bc3
harrismawardi <harris.mawardi@yahoo.co.uk>
2021-10-14 18:01:29 +0100

    adding getbooklists helper function into react list components

--- client/src/pages/CurrentReads/index.js
+++ client/src/pages/CurrentReads/index.js
@@ -1,13 +1,24 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { Nav, CurrentReadCard } from "../../components";
 import { checkToken } from "../../actions/loginauth";
+import { getBooksList } from "../../actions/helpers";
 
 const CurrentReads = () => {
   checkToken()
+  const currentList = getBooksList("current")
+  console.log(currentList)
+  const [currentBookElements, setCurrentBookElements] = useState([])
+
+  useEffect(() => {
+    const current = currentList.map((book) => <CurrentReadCard />)
+    setCurrentBookElements(current)
+  }, [])
+  getBooksList("current")
+
   return (
     <div className="body">
       <p>Current Reads</p>
-      <CurrentReadCard />
+      {currentBookElements}
       <Nav />
     </div>
   );
--- client/src/pages/ToBeRead/index.js
+++ client/src/pages/ToBeRead/index.js
@@ -1,19 +1,26 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import "./styles.css";
 import { checkToken } from "../../actions/loginauth";
 import { LibraryCard, Nav } from "../../components/";
+import { getBooksList } from "../../actions/helpers";
 
 const ToBeRead = () => {
 
   checkToken()
+  const tbrList = getBooksList("tbr")
+  console.log(tbrList)
+  const [tbrElements, setTbrElements] = useState([])
 
-
+  useEffect(() => {
+    const tbr = tbrList.map((book) => <LibraryCard />)
+    setTbrElements(tbr)
+  }, [])
 
   return (
     <div className="body">
       <Nav />
       <div className="cardContainer">
-        {/* <LibraryCard title= cover= author=/> */}
+        {tbrElements}
       </div>
     </div>
   );
