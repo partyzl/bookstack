449cb0741dc12324845475c45ba66b0473e06764
jasminappleby <jasminapplebyholdham@hotmail.com>
2021-10-14 12:17:04 +0100

    added /completedreads route and basic page layout

---
+++ client/src/pages/CompletedReads/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Nav, Rating, BookCardNoBtn, CurrentReadCard, Button } from "../../components";
+import { useHistory } from "react-router";
+import { checkToken } from "../../actions/loginauth";
+
+const CompletedReads = () => {
+    let history = useHistory();
+
+    const editReview = () => {
+      history.push("/review");
+    };
+    // https://bookstack-heroku-app.herokuapp.com/books/
+  checkToken()
+  return (
+    <div className="body">
+      <p>Completed books</p>
+      <BookCardNoBtn />
+      <form>
+        <label>
+            Review: 
+        <input type="text" name="review" maxLength="280" disabled/>
+        </label>
+        <Rating disabled/>
+        
+            <div>
+            <Button type="button" className={"btn btn-primary"} onClick={editReview}>
+                <i className="bi bi-plus-circle"></i> Edit review
+            </Button>
+            </div>
+        </form>
+      <Nav />
+    </div>
+  );
+};
+export default CompletedReads;
